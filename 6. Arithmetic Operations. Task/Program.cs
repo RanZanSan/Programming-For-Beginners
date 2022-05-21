30876f0c7ae40c470a0a9b28a6a20ce127ba0d4c
Rasskazov Andrey <ranzansan@gmail.com>
2022-05-21 23:00:26 +0300

    Arithmetic

---
+++ 6. Arithmetic Operations. Task/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Arithmetic_Operations.Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(81 / 9);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 6. Arithmetic Operations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Arithmetic_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(3 + 4);
+            Console.ReadKey();
+
+            Console.WriteLine(8 / 2);
+            Console.WriteLine(3 * 3 * 3);
+            Console.ReadKey();
+        }
+    }
+}
